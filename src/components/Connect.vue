4091d32459e8a9bfdcb3dab04248cb0346e1d4a7
Denzyl Dick <ddenzyl@gmail.com>
2026-07-29 13:04:46 +0100

    fix: QR code now only visible after pressing Host or Join button

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -34,69 +34,108 @@
           {{ $t('connect.link_device_desc') }}
         </div>
 
-        <ConnectModeToggle v-if="!hideModeToggle" v-model="mode" />
-
-        <ConnectHostView
-          v-if="mode === 'host'"
-          :uuid="uuid"
-          :passphrase="passphrase"
-          :peer-joined="peerJoined"
-          :is-connected="isConnected"
-        />
-
-        <ConnectJoinView
-          v-if="mode === 'join' && selectedLanHost"
-          v-model="joinPassphrase"
-          :loading="loading"
-          :is-connected="isConnected"
-          :show-sync-button="isConnected"
-          :syncing="syncing"
-          @join="joinWebRTC"
-          @sync="triggerSync"
-        />
-
-        <ConnectLanDiscovery
-          v-if="mode === 'join' && !selectedLanHost && !isConnected"
-          @select="selectLanHost"
-        />
-
-        <div
-          v-if="mode === 'host' && isConnected && syncing"
-          class="d-flex justify-center mb-6"
-        >
-          <v-progress-linear
-            indeterminate
-            color="success"
-            height="4"
-          />
-        </div>
-
-        <div
-          v-if="peerList.length > 0"
-          class="text-left px-2 mb-2"
-        >
-          <div class="text-caption font-weight-bold text-zinc-secondary mb-1">
-            {{ $t('devices.connected') }} ({{ peerList.length }})
-          </div>
-          <div
-            v-for="peer in peerList"
-            :key="peer.device_id"
-            class="d-flex align-center pa-2 mb-1 rounded bg-zinc-50"
+        <div v-if="!started" class="d-flex flex-column align-center mb-6 ga-4">
+          <v-btn
+            color="black"
+            variant="flat"
+            height="56"
+            class="siegu-btn px-8 text-none"
+            @click="start('host')"
           >
-            <v-icon size="14" class="mr-2 text-zinc-secondary">mdi-laptop</v-icon>
-            <span class="text-body-2 text-zinc-primary font-weight-medium">{{ peer.name }}</span>
-            <span class="text-caption text-zinc-secondary ml-2">{{ peer.os }}</span>
+            <div class="d-flex align-center">
+              <div class="siegu-icon-circle siegu-icon-circle-md mr-3">
+                <v-icon size="16">mdi-laptop</v-icon>
+              </div>
+              <span class="font-weight-bold">{{ $t('connect.host') }}</span>
+            </div>
+          </v-btn>
+          <div class="text-caption text-zinc-muted text-center px-4">
+            {{ $t('connect.host_desc') }}
+          </div>
+          <v-btn
+            color="black"
+            variant="flat"
+            height="56"
+            class="siegu-btn px-8 text-none"
+            @click="start('join')"
+          >
+            <div class="d-flex align-center">
+              <div class="siegu-icon-circle siegu-icon-circle-md mr-3">
+                <v-icon size="16">mdi-cellphone-link</v-icon>
+              </div>
+              <span class="font-weight-bold">{{ $t('connect.join') }}</span>
+            </div>
+          </v-btn>
+          <div class="text-caption text-zinc-muted text-center px-4">
+            {{ $t('connect.join_desc') }}
           </div>
         </div>
 
-        <ConnectStatusBar
-          :status="connectionStatus"
-          :is-connected="isConnected"
-          :sync-progress="syncProgress"
-          :show-disconnect="showDisconnect"
-          :disconnecting="disconnecting"
-          @disconnect="disconnectSession"
-        />
+        <template v-if="started">
+          <ConnectModeToggle v-if="!hideModeToggle" v-model="mode" />
+
+          <ConnectHostView
+            v-if="mode === 'host'"
+            :uuid="uuid"
+            :passphrase="passphrase"
+            :peer-joined="peerJoined"
+            :is-connected="isConnected"
+          />
+
+          <ConnectJoinView
+            v-if="mode === 'join' && selectedLanHost"
+            v-model="joinPassphrase"
+            :loading="loading"
+            :is-connected="isConnected"
+            :show-sync-button="isConnected"
+            :syncing="syncing"
+            @join="joinWebRTC"
+            @sync="triggerSync"
+          />
+
+          <ConnectLanDiscovery
+            v-if="mode === 'join' && !selectedLanHost && !isConnected"
+            @select="selectLanHost"
+          />
+
+          <div
+            v-if="mode === 'host' && isConnected && syncing"
+            class="d-flex justify-center mb-6"
+          >
+            <v-progress-linear
+              indeterminate
+              color="success"
+              height="4"
+            />
+          </div>
+
+          <div
+            v-if="peerList.length > 0"
+            class="text-left px-2 mb-2"
+          >
+            <div class="text-caption font-weight-bold text-zinc-secondary mb-1">
+              {{ $t('devices.connected') }} ({{ peerList.length }})
+            </div>
+            <div
+              v-for="peer in peerList"
+              :key="peer.device_id"
+              class="d-flex align-center pa-2 mb-1 rounded bg-zinc-50"
+            >
+              <v-icon size="14" class="mr-2 text-zinc-secondary">mdi-laptop</v-icon>
+              <span class="text-body-2 text-zinc-primary font-weight-medium">{{ peer.name }}</span>
+              <span class="text-caption text-zinc-secondary ml-2">{{ peer.os }}</span>
+            </div>
+          </div>
+
+          <ConnectStatusBar
+            :status="connectionStatus"
+            :is-connected="isConnected"
+            :sync-progress="syncProgress"
+            :show-disconnect="showDisconnect"
+            :disconnecting="disconnecting"
+            @disconnect="disconnectSession"
+          />
+        </template>
 
         <v-divider class="opacity-10 my-4"></v-divider>
 
@@ -114,31 +153,69 @@
     </v-dialog>
 
     <div v-if="embedded" class="w-100">
-      <ConnectModeToggle v-if="!hideModeToggle" v-model="mode" />
+      <div v-if="!started && !hideModeToggle" class="d-flex flex-column align-center mb-6 ga-4">
+        <v-btn
+          color="black"
+          variant="flat"
+          height="56"
+          class="siegu-btn px-8 text-none"
+          @click="start('host')"
+        >
+          <div class="d-flex align-center">
+            <div class="siegu-icon-circle siegu-icon-circle-md mr-3">
+              <v-icon size="16">mdi-laptop</v-icon>
+            </div>
+            <span class="font-weight-bold">{{ $t('connect.host') }}</span>
+          </div>
+        </v-btn>
+        <div class="text-caption text-zinc-muted text-center px-4">
+          {{ $t('connect.host_desc') }}
+        </div>
+        <v-btn
+          color="black"
+          variant="flat"
+          height="56"
+          class="siegu-btn px-8 text-none"
+          @click="start('join')"
+        >
+          <div class="d-flex align-center">
+            <div class="siegu-icon-circle siegu-icon-circle-md mr-3">
+              <v-icon size="16">mdi-cellphone-link</v-icon>
+            </div>
+            <span class="font-weight-bold">{{ $t('connect.join') }}</span>
+          </div>
+        </v-btn>
+        <div class="text-caption text-zinc-muted text-center px-4">
+          {{ $t('connect.join_desc') }}
+        </div>
+      </div>
 
-      <ConnectHostView
-        v-if="mode === 'host'"
-        :uuid="uuid"
-        :passphrase="passphrase"
-        :peer-joined="peerJoined"
-        :is-connected="isConnected"
-      />
+      <template v-if="started">
+        <ConnectModeToggle v-if="!hideModeToggle" v-model="mode" />
 
-      <ConnectJoinView
-        v-if="mode === 'join' && selectedLanHost"
-        v-model="joinPassphrase"
-        :loading="loading"
-        :is-connected="isConnected"
-        :show-sync-button="false"
-        :syncing="syncing"
-        @join="joinWebRTC"
-        @sync="triggerSync"
-      />
+        <ConnectHostView
+          v-if="mode === 'host'"
+          :uuid="uuid"
+          :passphrase="passphrase"
+          :peer-joined="peerJoined"
+          :is-connected="isConnected"
+        />
 
-      <ConnectLanDiscovery
-        v-if="mode === 'join' && !selectedLanHost && !isConnected"
-        @select="selectLanHost"
-      />
+        <ConnectJoinView
+          v-if="mode === 'join' && selectedLanHost"
+          v-model="joinPassphrase"
+          :loading="loading"
+          :is-connected="isConnected"
+          :show-sync-button="false"
+          :syncing="syncing"
+          @join="joinWebRTC"
+          @sync="triggerSync"
+        />
+
+        <ConnectLanDiscovery
+          v-if="mode === 'join' && !selectedLanHost && !isConnected"
+          @select="selectLanHost"
+        />
 
         <div
           v-if="peerList.length > 0"
@@ -170,6 +247,7 @@
         <v-icon v-else color="success" size="16" class="mr-2">mdi-check-circle-outline</v-icon>
         {{ connectionStatus }}
       </div>
+      </template>
     </div>
   </div>
 </template>
@@ -203,6 +281,7 @@ const emit = defineEmits<{
 }>()
 
 const dialog = ref(false)
+const started = ref(false)
 const {
   mode,
   uuid,
@@ -237,13 +316,17 @@ const showDisconnect = computed(() => {
   )
 })
 
+function start(selectedMode: 'host' | 'join') {
+  started.value = true
+  mode.value = selectedMode
+  initialize()
+}
+
 watch(mode, async (newMode, oldMode) => {
   emit('modeChange', newMode)
-  if (oldMode && oldMode !== newMode) {
+  if (started.value && oldMode && oldMode !== newMode) {
     await disconnectSession()
     resetJoinState()
-  }
-  if (newMode === 'host') {
     initialize()
   }
 })
@@ -274,8 +357,8 @@ watch(syncProgress, (progress) => {
 watch(dialog, async (open) => {
   if (open) {
     await startEventListeners()
-    initialize()
   } else {
+    started.value = false
     await disconnectSession()
     stopEventListeners()
     loading.value = false
@@ -290,8 +373,8 @@ onMounted(() => {
   mode.value = props.initialMode
   if (props.embedded) {
     startEventListeners()
-    if (mode.value === 'host') {
-      initialize()
+    if (props.hideModeToggle) {
+      start(props.initialMode)
     }
   }
 })
